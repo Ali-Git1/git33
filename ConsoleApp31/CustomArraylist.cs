d060d5b0855112a0b63d5a748eeedcb90cca4b1e
Ali Aliyev <elvinaliyev724@gmail.com>
2024-04-25 20:52:24 +0400

    finish

---
+++ ConsoleApp31/CustomArraylist.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    public class CustomArraylist<T>
+    {
+        public T[] array;
+        public int capacity;
+        public int count;
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public CustomArraylist(int capacity)
+        {
+            this.capacity = capacity;
+            array = new T[capacity];
+            count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (Count == Capacity)
+            {
+                Array.Resize(ref array, capacity *= 2);
+            }
+            array[count++] = item;
+        }
+
+        public void TrimToSize()
+        {
+            if (Count < Capacity)
+            {
+                Array.Resize(ref array, count);
+                capacity = count;
+            }
+        }
+
+        public void Remove(object item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (object.Equals(array[i], item))
+                {
+                    RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            while (index < Count - 1)
+            {
+                array[index] = array[index + 1];
+                index++;
+            }
+            count--;
+            array[Count] = default(T); 
+        }
+
+        public void AddRange(T[] collection)
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+    }
+
+
+}
+
---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                CustomArraylist<string> customList = new CustomArraylist<string>(5);
+
+                
+                customList.Add("alma");
+                customList.Add("banan");
+                customList.Add("mandarin");
+                customList.Add("Mango");
+                customList.Add("uzum");
+
+                string[] moreFruits = { "alma", "yemis", "fruit" };
+                customList.AddRange(moreFruits);
+
+                
+                customList.Remove("banan");
+
+                
+                customList.RemoveAt(3);
+
+               
+                customList.TrimToSize();
+
+               
+               
+                for (int i = 0; i < customList.Count; i++)
+                {
+                    Console.WriteLine(customList.array[i]);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+    
+}
+
